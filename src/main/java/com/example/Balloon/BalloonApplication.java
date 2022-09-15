a41786b4c51cb66ab4793bf5ed9979d7929f1978
Mirko Petrovic <mirkic.kg99@gmail.com>
2022-09-15 14:29:52 +0200

    C:/Program Files/Git/

--- src/main/java/com/example/Balloon/BalloonApplication.java
+++ src/main/java/com/example/Balloon/BalloonApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BalloonApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BalloonApplication.class, args);
 	}
