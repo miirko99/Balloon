eba03e83355edcbdf8aef455363cd04d1b4f0912
Mirko Petrovic <mirkic.kg99@gmail.com>
2022-09-15 10:21:34 +0200

    created files by spring initializr

---
+++ src/test/java/com/example/Balloon/BalloonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Balloon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalloonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
